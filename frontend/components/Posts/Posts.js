e8444f288bd21d54e19769a44af96904cde6017e
Whitneyy <whitneyyx77@gmail.com>
2024-08-12 01:28:09 -0400

    Edited App.js, Comments.js, LikeSection.js, Post.js, and Posts.js files to complete each task for the project

--- frontend/components/Posts/Posts.js
+++ frontend/components/Posts/Posts.js
@@ -7,7 +7,13 @@ const Posts = (props) => {
 
   return (
     <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(post => (
+        <Post
+        key={post.id}
+        post={post}
+        likePost={likePost}
+        />
+      ))} {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
